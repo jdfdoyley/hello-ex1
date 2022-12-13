beeddf201ee34a875debed99a62fe152d7967fed
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-12 20:06:48 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-    //     Code here...
+        System.out.println("Hello");
+        System.out.println("Jason D'Oyley");
     }
 }

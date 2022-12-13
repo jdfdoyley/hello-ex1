ae540c324a2fd62551064577314b1c17b9bc1777
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-12 20:04:35 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Write a Java program to print 'Hello' on screen and then print your name
+ * on a separate line.
+ * Expected Output:
+ * Hello
+ * Alexandra Abramov
+ */
+public class Main {
+    public static void main(String[] args) {
+    //     Code here...
+    }
+}

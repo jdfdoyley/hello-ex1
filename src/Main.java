13fe8c323bcdfdbb8257d2965f572aa2290bd0c2
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-12 20:10:14 -0500

    Print text to the screen according to the output

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Jason D'Oyley");
+        System.out.println("Hello\nJason D'Oyley");
     }
 }
